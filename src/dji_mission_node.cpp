3bfb28be7be2d55d5bd2ec1afe5e367f1b87ac31
miggilcas <miguel.gil2000@gmail.com>
2023-07-31 16:42:25 +0200

    recording actions upgraded

--- src/dji_mission_node.cpp
+++ src/dji_mission_node.cpp
@@ -211,9 +211,9 @@ createWaypoints(std::vector<sensor_msgs::NavSatFix> gpsList, std_msgs::Float64Mu
   start_wp.altitude  = start_alt;
   start_wp.hasAction = 1;
   start_wp.actionNumber= 1;
-  start_wp.actionTimeLimit = 100;
+  start_wp.actionTimeLimit = 6000;
   start_wp.commandList[0] = 2; //start recording
-
+  start_wp.commandParameter[1] = 1;
   ROS_INFO("Waypoint created at (LLA): %f \t%f \t%f\n", gps_pos.latitude,
            gps_pos.longitude, start_alt);
 
@@ -241,8 +241,11 @@ createWaypoints(std::vector<sensor_msgs::NavSatFix> gpsList, std_msgs::Float64Mu
     wp.commandList[0] = 5; // WP_ACTION_STAY= 0,  WP_ACTION_SIMPLE_SHOT= 1,  WP_ACTION_VIDEO_START= 2,  WP_ACTION_VIDEO_STOP= 3,
                            // WP_ACTION_CRAFT_YAW = 4,  WP_ACTION_GIMBAL_PITCH         = 5
     wp.commandParameter[0] = gimbalPitchList.data[i];
-    if (wp.index = gpsList.size()) wp.commandList[1] = 3; //stop recording if we finish the mission
-    // Turn mode values:  0: clockwise, 1: counter-clockwise 
+    if (wp.index = gpsList.size()){
+      wp.actionTimeLimit = 6000;
+      wp.commandList[1] = 3; //stop recording if we finish the mission
+      wp.commandParameter[1] = 1;
+    } // Turn mode values:  0: clockwise, 1: counter-clockwise 
     if (wp.yaw < yawList.data[i+1] && wp.index <= gpsList.size())
       wp.turnMode           = 0; // depends on the yaw
     else{
