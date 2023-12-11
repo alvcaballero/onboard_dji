03329d07686dfadf9d5575d66371cc03d7c709d6
miggilcas <miguel.gil2000@gmail.com>
2023-12-11 16:51:35 +0100

    Error debugging khadas

--- src/dji_waypointV2_node.cpp
+++ src/dji_waypointV2_node.cpp
@@ -226,10 +226,10 @@ bool WaypointV2actionsAutomated(ros::NodeHandle &nh, uint16_t actionNum)
         action->waypointV2AssociateTrigger.waitingTime = 0;
         action->waypointV2AssociateTrigger.actionIdAssociated = id-1;
 
-        actionVector_camera.waypointV2ACtionActuatorType = dji_osdk_ros::WaypointV2Action::DJIWaypointV2ActionActuatorTypeCamera;
-        actionVector_camera.waypointV2CameraActuator.actuatorIndex = 0;
-        actionVector_camera.waypointV2CameraActuator.DJIWaypointV2ActionActuatorCameraOperationType = dji_osdk_ros::WaypointV2CameraActuator::DJIWaypointV2ActionActuatorCameraOperationTypeStartRecordVideo;
-        generateWaypointV2Action_.request.actions.push_back(actionVector_camera);
+        action.waypointV2ACtionActuatorType = dji_osdk_ros::WaypointV2Action::DJIWaypointV2ActionActuatorTypeCamera;
+        action.waypointV2CameraActuator.actuatorIndex = 0;
+        action.waypointV2CameraActuator.DJIWaypointV2ActionActuatorCameraOperationType = dji_osdk_ros::WaypointV2CameraActuator::DJIWaypointV2ActionActuatorCameraOperationTypeStartRecordVideo;
+        generateWaypointV2Action_.request.actions.push_back(action);
         
         ROS_INFO("Start recording action created with ID: %d associated to action: %d ", action->actionId, action->waypointV2AssociateTrigger.actionIdAssociated); // add more info when advances come
 
